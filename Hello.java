ee2a9d37ee3a04d3e5e33b9857304f1fbac4a900
Joe Plt <empous@mail.com>
2021-09-28 23:05:02 -0400

    last

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 	public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hellhhhhfhhofff fpppprom Java");
+        System.out.println("Helgfgfgflhhhhfhhofff fpppprom Java");
     }
 }
