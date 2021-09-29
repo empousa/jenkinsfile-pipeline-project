692bec3aafca45f14973bb621492599dc8c21e39
Joe Plt <empous@mail.com>
2021-09-28 22:52:19 -0400

    reviewd

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 	public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hellhfhhofff fpppprom Java");
+        System.out.println("Hellhhhhfhhofff fpppprom Java");
     }
 }
